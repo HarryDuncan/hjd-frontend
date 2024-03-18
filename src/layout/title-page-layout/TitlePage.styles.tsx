00900abf1854917cb0410e5767a544a4122eeb54
Harry Duncan <h.duncan@live.com>
2024-03-18 15:34:36 +1100

    feature: smooth loading

--- src/layout/title-page-layout/TitlePage.styles.tsx
+++ src/layout/title-page-layout/TitlePage.styles.tsx
@@ -13,11 +13,11 @@ export const PageTitleContainer = styled.div`
   width: 100vw;
   position: relative;
   top: 0;
-  height: 20vh;
+  height: 0vh;
   background: ${({ theme }) => theme.colors.gradients.dark};
   z-index: 50;
   ${TextScrollerText} {
     font-size: ${({ theme }) => theme.font.size.large};
   }
-  animation: ${growDown} 1.5s ease-in-out 0s forwards;
+  animation: ${growDown} 1s ease-in-out 1.5s forwards;
 `;
