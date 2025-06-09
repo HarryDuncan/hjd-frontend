c5d6688913574b0f2ae383930af974ef89874fde
Harry Duncan <h.duncan@live.com>
2025-06-10 06:10:03 +1000

    fix link tree

--- src/views/link-tree/LinkTree.tsx
+++ src/views/link-tree/LinkTree.tsx
@@ -1,23 +1,26 @@
 import { MainTitle } from "components/text/Text";
 import { LinkTreeLink } from "models/link-tree/types";
-import { LinkTreeItem } from "./LinkTree.styles";
 import {
-  FloatingCentralContainer,
-  OverlayDiv,
-} from "components/containers/Containers";
+  LinkItemOverlay,
+  LinkItemsContainer,
+  LinkTreeItem,
+} from "./LinkTree.styles";
+import { FloatingCentralContainer } from "components/containers/Containers";
 
 interface LinkTreeProps {
   links: LinkTreeLink[];
 }
 export const LinkTree = ({ links }: LinkTreeProps) => (
   <FloatingCentralContainer>
-    <OverlayDiv />
-    {links.map(({ url, title }) => (
-      <LinkTreeItem>
-        <a href={url}>
-          <MainTitle $isLight>{title}</MainTitle>
-        </a>
-      </LinkTreeItem>
-    ))}
+    <LinkItemsContainer>
+      {links.map(({ url, title }) => (
+        <LinkTreeItem>
+          <LinkItemOverlay />
+          <a href={url}>
+            <MainTitle $isLight>{title}</MainTitle>
+          </a>
+        </LinkTreeItem>
+      ))}
+    </LinkItemsContainer>
   </FloatingCentralContainer>
 );
